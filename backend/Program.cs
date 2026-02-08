384df05585f63accd8984fca79274b208e20546f
Max Assermark <max.assermark@gmail.com>
2026-02-08 19:07:30 +0100

    add db migrate on startup

--- backend/Program.cs
+++ backend/Program.cs
@@ -43,6 +43,12 @@ builder.Services.AddCors(options =>
 
 var app = builder.Build();
 
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    db.Database.Migrate();
+}
+
 app.UseMiddleware<ApiExceptionHandlerMiddleware>();
 app.UseCors();
 app.UseAuthentication();
